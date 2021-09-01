74616e099a87f5e486bdd28eb80f0bd74392d9ce
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-09-01 12:23:55 +0600

    Update examples.py

--- examples.py
+++ examples.py
@@ -25,19 +25,38 @@ client.auth(phone=phonenumber, password=password)
 #follow user
 import aminolab
 client = aminolab.Client()
-client.auth(email=email, password=password)
+client.auth(email="email", password="password")
 client.follow_user(ndcId="ndcId", followee_id="userId")
 
 #unfollow_user
 import aminolab
 client = aminolab.Client()
-client.auth(email=email, password=password)
+client.auth(email="email", password="password")
 client.unfollow_user(ndcId="ndcId", followee_id="userId")
 
+#get public communities list
+import aminolab
+client = aminolab.Client()
+client.auth(email="email", password="password")
+clients = client.get_public_communities(language="en", size=100)
+for x, name in enumerate(clients.name, 1):
+	print(f"{x}.{name}")
+ndcId = clients.ndcId[int(input("Select the community >> "))-1]
+
+#get joined communities list
+import aminolab
+client = aminolab.Client()
+client.auth(email="email", password="password")
+clients = client.my_communities()
+for x, name in enumerate(clients.name, 1):
+	print(f"{x}.{name}")
+ndcId = clients.ndcId[int(input("Select the community >> "))-1]
+
+client.auth
 #get public chats list
 import aminolab
 client = aminolab.Client()
-client.auth(email=email, password=password)
+client.auth(email="email", password="password")
 chats = client.get_public_chat_threads(ndcId="ndcId", size="size")
 for z, title in enumerate(chats.title, 1):
 	print(f"{z}.{title}")
@@ -46,7 +65,7 @@ chatx = chats.threadId[int(input("Select The Chat >> "))-1]
 #get joined chats list
 import aminolab
 client = aminolab.Client()
-client.auth(email=email, password=password)
+client.auth(email="email", password="password")
 chats = client.my_chat_threads(ndcId="ndcId", size="size")
 for z, title in enumerate(chats.title, 1):
 	print(f"{z}.{title}")
