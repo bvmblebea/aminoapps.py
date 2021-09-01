355a6361429f72840595fc42958ad54140387a73
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-09-01 12:15:53 +0600

    added new functions
    added new functions
    1.get_public_communities
    2.my_communities
    3.get_from_link
    4.leave_community
    5.get_online_members
    6.logout
    deleted functions
    1.members_in_thread

--- aminolab.py
+++ aminolab.py
@@ -1,6 +1,5 @@
 #Library In Development created by LilZevi
 #I have not tested some functions. 
-#I tested only auth, follow_user, unfollow_user
 import requests
 import json
 import random
@@ -10,6 +9,7 @@ from utils import headers, objects
 class Client():
 	def __init__(self, deviceId: str = "22717F5C01029F06DAED62B82F001AAB42333CD930C7936EC7B253594887BA6CE6820148ED69CBF2D0"):
 		self.api = "https://aminoapps.com/api"
+		self.api_p = "https://aminoapps.com/api-p"
 		self.headers = headers.Headers().headers
 		self.deviceId = deviceId
 		self.userId = None
@@ -41,16 +41,27 @@ class Client():
 		self.headers = headers.Headers(sid=self.sid).headers
 		return request.json()
 	
+	#logout
+	def logout(self):
+		self.headers = {"cookie": f"sid={self.sid.replace('sid=', '')}; Expires=Thu, 01-Jan-1970 00:00:00 GMT; Path=/"}
+		request = requests.post("{self.api}/logout", headers=self.headers)
+		self.sid = None
+		self.userId = None
+		request.headers["cookie"] = request.headers["set-cookie"]
+		request.headers.pop("set-cookie")
+		self.headers = request.headers
+		return request.json()
+
 	#get public chats list
 	def get_public_chat_threads(self, ndcId, start: int = 0, size: int = 10):
 		request = requests.get(f"{self.api}/chat/live-threads?ndcId=x{ndcId}&start={start}&size={size}", headers=self.headers).json()
-		return objects.chatThreads(request["result"]["threadList"]).chatThreads
+		return objects.ChatThreads(request["result"]["threadList"]).ChatThreads
 
 	#get joined chats list
 	def my_chat_threads(self, ndcId, start: int = 0, size: int = 10):
 		data = {"ndcId": f"x{ndcId}", "start": start, "size": size}
 		request = requests.post(f"{self.api}/my-chat-threads", json=data, headers=self.headers).json()
-		return objects.chatThreads(request["result"]["threadList"]).chatThreads
+		return objects.ChatThreads(request["result"]["threadList"]).ChatThreads
 		
 	#send_message
 	def send_message(self, ndcId, threadId, message: str = None, messageType: int = 0):
@@ -88,27 +99,30 @@ class Client():
 		request = requests.post(f"{self.api}/leave-thread", json=data, headers=self.headers)
 		return request.json()
 	
-	#get chat users
-	def members_in_thread(self, ndcId, threadId, start: int = 0, size: int = 10):
-		data = {
-		"ndcId": f"x{ndcId}",
-		"size": size,
-		"start": start,
-		"threadId": threadId,
-		"type": "default"
-		}
-		request = requests.get(f"{self.api}/members-in-thread", json=data, headers=self.headers)
-		return request.json()
+	#get online users
+	def get_online_members(self, ndcId: str):
+		request = requests.get(f"{self.api}/x{ndcId}/online-members", headers=self.headers).json()
+		return objects.MembersList(request["result"]["onlineMembersList"]).MembersList
 	
+	#get public communities list, languages - ru = Russia, en = English
+	def get_public_communities(self, language: str, size: int = 25):
+		request = requests.get(f"{self.api_p}/g/s/topic/0/feed/community?language={language}&type=web-explore&categoryKey=recommendation&size={size}&pagingType=t", headers=self.headers).json()
+		return objects.CommunityList(request["communityList"]).CommunityList
+	
+	#get joined communities list
+	def my_communities(self):
+		request = requests.get(f"{self.api_p}/g/s/community/joined", headers=self.headers).json()
+		return objects.CommunityList(request["communityList"]).CommunityList
+		
 	#follow
-	def follow_user(self, ndcId, followee_id: str):
-		data = {"followee_id": followee_id, "ndcId": f"x{ndcId}"}
+	def follow_user(self, ndcId, userId: str):
+		data = {"followee_id": userId, "ndcId": f"x{ndcId}"}
 		request = requests.post(f"{self.api}/follow-user", json=data, headers=self.headers)
 		return request.json()
 	
 	#unfollow
-	def unfollow_user(self, ndcId, followee_id: str):
-		data = {"followee_id": followee_id, "follower_id": self.userId, "ndcId": f"x{ndcId}"}
+	def unfollow_user(self, ndcId, userId: str):
+		data = {"followee_id": userId, "follower_id": self.userId, "ndcId": f"x{ndcId}"}
 		request = requests.post(f"{self.api}/unfollow-user", json=data, headers=self.headers)
 		return request.json()
 	
@@ -147,6 +161,12 @@ class Client():
 		request = requests.post(f"{self.api}/join", json=data, headers=self.headers)
 		return request.json()
 	
+	#leave community
+	def leave_community(self, ndcId: str):
+		data = {"ndcId": ndcId}
+		request = requests.post(f"{self.api}/leave", json=data, headers=self.headers)
+		return request.json()
+		
 	#request join community
 	def request_join_community(self, ndcId, message: str = None):
 		data = {"message": message, "ndcId": ndcId}
@@ -163,12 +183,18 @@ class Client():
 		request = requests.post(f"{self.api}/add-flag", json=data, headers=self.headers)
 		return request.json()
 	
-	#send_active_object. In development!
+	#send_active_object. VERY SLOW FUCK 
 	def send_active_object(self, ndcId):
 		data = {"ndcId": ndcId}
 		request = requests.post(f"{self.api}/community/stats/web-user-active-time", json=data, headers=self.headers)
 		return request.json()
-	
+		
 	#get websocket url
 	def get_web_socket_url(self):
 		request = requests.get(f"{self.api}/chat/web-socket-url", headers=self.headers)
+		return request.json()
+		
+	#get link information
+	def get_from_link(self, link: str):
+		request = requests.get(f"{self.api_p}/g/s/link-resolution?q={link}", headers=self.headers).json()
+		return objects.FromLink(request["linkInfoV2"]).FromLink
