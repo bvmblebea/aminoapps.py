3659513c5fad22875831c65094a01833a162f521
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-09-05 12:19:48 +0800

    Update examples.py

--- src/examples.py
+++ src/examples.py
@@ -26,13 +26,13 @@ client.auth(phone=phonenumber, password=password)
 import AminoLab
 client = AminoLab.Client()
 client.auth(email="email", password="password")
-client.follow_user(ndcId="ndcId", followee_id="userId")
+client.follow_user(ndc_Id="ndc_Id", user_Id="user_Id")
 
 #unfollow_user
 import AminoLab
 client = AminoLab.Client()
 client.auth(email="email", password="password")
-client.unfollow_user(ndcId="ndcId", followee_id="userId")
+client.unfollow_user(ndc_Id="ndc_Id", user_Id="user_Id")
 
 #get public communities list
 import AminoLab
@@ -41,7 +41,7 @@ client.auth(email="email", password="password")
 clients = client.get_public_communities(language="en", size=100)
 for x, name in enumerate(clients.name, 1):
 	print(f"{x}.{name}")
-ndcId = clients.ndcId[int(input("Select the community >> "))-1]
+ndc_Id = clients.ndc_Id[int(input("Select the community >> "))-1]
 
 #get joined communities list
 import AminoLab
@@ -50,22 +50,22 @@ client.auth(email="email", password="password")
 clients = client.my_communities()
 for x, name in enumerate(clients.name, 1):
 	print(f"{x}.{name}")
-ndcId = clients.ndcId[int(input("Select the community >> "))-1]
+ndc_Id = clients.ndc_Id[int(input("Select the community >> "))-1]
 
 #get public chats list
 import AminoLab
 client = AminoLab.Client()
 client.auth(email="email", password="password")
-chats = client.get_public_chat_threads(ndcId="ndcId", size="size")
+chats = client.get_public_chat_threads(ndc_Id="ndc_Id", size="size")
 for z, title in enumerate(chats.title, 1):
 	print(f"{z}.{title}")
-chatx = chats.threadId[int(input("Select The Chat >> "))-1]
+thread_Id = chats.thread_Id[int(input("Select The Chat >> "))-1]
 
 #get joined chats list
 import AminoLab
 client = AminoLab.Client()
 client.auth(email="email", password="password")
-chats = client.my_chat_threads(ndcId="ndcId", size="size")
+chats = client.my_chat_threads(ndc_Id="ndc_Id", size="size")
 for z, title in enumerate(chats.title, 1):
 	print(f"{z}.{title}")
-chatx = chats.threadId[int(input("Select The Chat >> "))-1]
+thread_Id = chats.thread_Id[int(input("Select The Chat >> "))-1]
