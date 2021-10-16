71d90c55c64cc89ceb15e1ffedf4965135cdcdfd
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-10-16 21:49:01 +0800

    Update headers.py

--- src/utils/headers.py
+++ src/utils/headers.py
@@ -1,13 +1,17 @@
 sid = None
 user_Id = None
+device_Id = None
 
 class Headers:
-    def __init__(self, sid: str = None):
+    def __init__(self, device_Id: str = None, sid: str = None):
         self.headers = {
-        	"cookie": sid,
+            "cookie": sid,
             "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/73.0.3683.86 Chrome/73.0.3683.86 Safari/537.36",
             "x-requested-with": "xmlhttprequest"
             }
-       	self.headers["set-cookie"] = sid 
-       	if user_Id:
-            	self.user_Id = user_Id
+        self.headers["set-cookie"] = sid 
+        if user_Id:
+                self.user_Id = user_Id
+      
+
+        self.headers_v2 = {"NDCDEVICEID": device_Id, "NDCAUTH": sid}
